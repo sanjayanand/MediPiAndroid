04a749c05b592f6d682b8152162c021cdb9181d3
sanjay.anand <sanjay.anand@airelogic.com>
2017-09-28 14:46:04 +0100

    added readings to store

--- app/src/main/java/com/spinecore/hack/medipiandroid/store/MediPiStorageUtils.java
+++ app/src/main/java/com/spinecore/hack/medipiandroid/store/MediPiStorageUtils.java
@@ -11,7 +11,7 @@ public class MediPiStorageUtils {
     public static final String OX_READING = "oximeter";
     public static final String WEIGHT_READING = "weight";
 
-    public static String getStringPreference(Context context, String key,String defaultValue) {
+    public static String getReading(Context context, String key,String defaultValue) {
         String value = null;
         SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME,0);
         if (preferences != null) {
@@ -20,7 +20,7 @@ public class MediPiStorageUtils {
         return value;
     }
 
-    public static boolean setStringPreference(Context context, String key, String value) {
+    public static boolean setReading(Context context, String key, String value) {
         SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME,0);
         if (preferences != null && !TextUtils.isEmpty(key)) {
             SharedPreferences.Editor editor = preferences.edit();
@@ -30,74 +30,4 @@ public class MediPiStorageUtils {
         return false;
     }
 
-    public static float getFloatPreference(Context context, String key, float defaultValue) {
-        float value = defaultValue;
-        SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME,0);
-        if (preferences != null) {
-            value = preferences.getFloat(key, defaultValue);
-        }
-        return value;
-    }
-
-    public static boolean setFloatPreference(Context context, String key, float value) {
-        SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME,0);
-        if (preferences != null) {
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putFloat(key, value);
-            return editor.commit();
-        }
-        return false;
-    }
-    public static long getLongPreference(Context context, String key, long defaultValue) {
-        long value = defaultValue;
-        SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME,0);
-        if (preferences != null) {
-            value = preferences.getLong(key, defaultValue);
-        }
-        return value;
-    }
-    public static boolean setLongPreference(Context context, String key, long value) {
-        SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME,0);
-        if (preferences != null) {
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putLong(key, value);
-            return editor.commit();
-        }
-        return false;
-    }
-    public static int getIntegerPreference(Context context, String key, int defaultValue) {
-        int value = defaultValue;
-        SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME,0);
-        if (preferences != null) {
-            value = preferences.getInt(key, defaultValue);
-        }
-        return value;
-    }
-    public static boolean setIntegerPreference(Context context, String key, int value) {
-        SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME,0);
-        if (preferences != null) {
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putInt(key, value);
-            return editor.commit();
-        }
-        return false;
-    }
-
-    public static boolean getBooleanPreference(Context context, String key, boolean defaultValue) {
-        boolean value = defaultValue;
-        SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME,0);
-        if (preferences != null) {
-            value = preferences.getBoolean(key, defaultValue);
-        }
-        return value;
-    }
-   public static boolean setBooleanPreference(Context context, String key, boolean value) {
-        SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME,0);
-        if (preferences != null) {
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putBoolean(key, value);
-            return editor.commit();
-        }
-        return false;
-    }
 }
